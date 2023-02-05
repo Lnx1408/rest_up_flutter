65d0b90b45b8b3d9b5fdb59a70e286ea912a4280
YERMIN YAIR LINO SANCHEZ <yermin.linos@ug.edu.ec>
2023-02-04 23:40:58 -0500

    Background

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -24,7 +24,6 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    ///Metodo para acceder a al menú principal
     void validarLogin(String email, String password) {
       if (email == "Grupo9" && password == "123456") {
         print('Bienvenido: $_email');
@@ -45,113 +44,124 @@ class _LoginState extends State<Login> {
     }
 
     return Scaffold(
-      // backgroundColor: Color.fromARGB(33, 33, 33, 200),
-      backgroundColor: const Color.fromARGB(255, 215, 242, 255),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Login',
-                style: TextStyle(fontSize: 40.0),
-              ),
-              const SizedBox(
-                width: 160.0,
-                height: 25.0,
-                child: Divider(color: Colors.transparent),
-              ),
-              const CircleAvatar(
-                radius: 100.0,
-                backgroundColor: Colors.transparent,
-                backgroundImage:
-                    AssetImage('assets/icons/rest_up_logo_black.png'),
-              ),
-              const SizedBox(
-                width: 160.0,
-                height: 15.0,
-                child: Divider(color: Colors.transparent),
-              ),
-              const Text(
-                'usuario: Grupo9',
-                style: TextStyle(fontSize: 15.0),
-              ),
-              const Text(
-                'password: 123456',
-                style: TextStyle(fontSize: 15.0),
-              ),
-              const SizedBox(
-                width: 160.0,
-                height: 25.0,
-                child: Divider(color: Colors.transparent),
-              ),
-              TextField(
-                // autofocus: true,
-                readOnly: false,
-                decoration: InputDecoration(
-                    hintText: 'Ingrese su usuario',
-                    labelText: 'Usuario',
-                    suffixIcon: const Icon(FontAwesomeIcons.solidUser),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0))),
-                onChanged: (valor) {
-                  _email = valor;
-                },
-              ),
-              const SizedBox(
-                height: 15.0,
-                child: Divider(color: Colors.transparent),
-              ),
-              TextField(
-                enableInteractiveSelection: false,
-                obscureText: true,
-                readOnly: false,
-                decoration: InputDecoration(
-                    hintText: 'Ingrese su contraseña',
-                    labelText: 'Contraseña',
-                    suffixIcon: const Icon(FontAwesomeIcons.solidEye),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0))),
-                onChanged: (valor) {
-                  _password = valor;
-                },
-              ),
-              const SizedBox(
-                width: 160.0,
-                height: 25.0,
-                child: Divider(color: Colors.transparent),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  // ignore: sort_child_properties_last
-                  child: const Text(
-                    'Ingresar',
-                    style: TextStyle(color: Colors.white70, fontSize: 25.0),
-                  ),
-                  onPressed: () {
-                    validarLogin(_email, _password);
+        // backgroundColor: Color.fromARGB(33, 33, 33, 200),
+        //backgroundColor: const Color.fromARGB(255, 215, 242, 255),
+        body: Stack(
+      children: [
+        const Image(image: AssetImage("assets/images/background.png")),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+          child: SizedBox(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 40.0),
+                ),
+                const SizedBox(
+                  width: 160.0,
+                  height: 25.0,
+                  child: Divider(color: Colors.transparent),
+                ),
+                const CircleAvatar(
+                  radius: 100.0,
+                  backgroundColor: Colors.transparent,
+                  backgroundImage:
+                      AssetImage('assets/icons/rest_up_logo_black.png'),
+                ),
+                const SizedBox(
+                  width: 160.0,
+                  height: 15.0,
+                  child: Divider(color: Colors.transparent),
+                ),
+                const Text(
+                  'usuario: Grupo9',
+                  style: TextStyle(fontSize: 15.0),
+                ),
+                const Text(
+                  'password: 123456',
+                  style: TextStyle(fontSize: 15.0),
+                ),
+                const SizedBox(
+                  width: 160.0,
+                  height: 25.0,
+                  child: Divider(color: Colors.transparent),
+                ),
+                TextField(
+                  // autofocus: true,
+                  readOnly: false,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(15),
+                      hintText: 'Ingrese su usuario',
+                      labelText: 'Usuario',
+                      suffixIcon: const Icon(FontAwesomeIcons.solidUser),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                  onChanged: (valor) {
+                    _email = valor;
                   },
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  // ignore: sort_child_properties_last
-                  child: const Text(
-                    'Salir',
-                    style: TextStyle(color: Colors.white70, fontSize: 25.0),
-                  ),
-                  onPressed: () {
-                    salirApp();
+                const SizedBox(
+                  height: 15.0,
+                  child: Divider(color: Colors.transparent),
+                ),
+                TextField(
+                  enableInteractiveSelection: false,
+                  obscureText: true,
+                  readOnly: false,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(15),
+                      fillColor: Colors.brown,
+                      hintText: 'Ingrese su contraseña',
+                      labelText: 'Contraseña',
+                      suffixIcon: const Icon(FontAwesomeIcons.solidEye),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                  onChanged: (valor) {
+                    _password = valor;
                   },
                 ),
-              )
-            ],
+                const SizedBox(
+                  width: 160.0,
+                  height: 25.0,
+                  child: Divider(color: Colors.transparent),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: colorPrimario),
+                    // ignore: sort_child_properties_last
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(color: Colors.white70, fontSize: 25.0),
+                    ),
+                    onPressed: () {
+                      validarLogin(_email, _password);
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: colorPrimario),
+                    // ignore: sort_child_properties_last
+                    child: const Text(
+                      'Salir',
+                      style: TextStyle(color: Colors.white70, fontSize: 25.0),
+                    ),
+                    onPressed: () {
+                      salirApp();
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ));
   }
 }
