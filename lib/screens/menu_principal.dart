86fd01837cd8da801cd5d267c6733d9a83c7f024
SERGIO VICENTE CAMPOVERDE LEIVA <sergio.compoverdel@ug.edu.ec>
2023-02-13 05:29:23 -0500

    cambios en el menu y login

--- lib/screens/menu_principal.dart
+++ lib/screens/menu_principal.dart
@@ -6,10 +6,10 @@ import 'package:rest_up_flutter/Classes/DatoRemplazo.dart';
 import 'package:rest_up_flutter/Templates/DesignApp.dart';
 import 'login.dart';
 
-void main() => runApp(const MenuPrincipal());
+//void main() => runApp(const MenuPrincipal());
 
 // ignore: must_be_immutable
-class MenuPrincipal extends StatefulWidget {
+/*class MenuPrincipal extends StatefulWidget {
   const MenuPrincipal({super.key});
 
   @override
@@ -68,6 +68,72 @@ class _MenuPrincipalState extends State<MenuPrincipal> {
           ),
         ),
       ),
+    );
+  }
+}
+*/
+////clase menu
+const bool flag = false;
+void main() => runApp(const MenuPrincipal());
+
+class MenuPrincipal extends StatelessWidget {
+  const MenuPrincipal({super.key});
+  static const titu = 'Principal';
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: flag,
+      title: titu,
+      home: Menu(title: titu),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  const Menu({super.key, required this.title}); //: super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+
+      // menu
+      body: const Center(child: Text('Nuevo')),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              //Navigator.push(
+              //context, MaterialPageRoute(builder, (context) => const MyApp())),
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )),
     );
   }
 }
